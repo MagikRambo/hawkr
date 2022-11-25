8ec67e03f80d2a756b2cb924c388061c18bd1d52
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2022-11-25 03:03:02 -0700

    loading map, but not functional

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,6 +1,11 @@
+import Map from "./Map"
+
 export default function Explore() {
     return (
-        <h1>This is Explore page</h1>
+        <>        <h1>This is Explore page</h1>
+        <Map />
+        </>
+
     )
 
 }
