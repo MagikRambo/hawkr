9dc530991a74d1165049f3585f2ad5256d85e24e
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2022-11-25 22:43:47 -0700

    cleaned up explore page

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,29 +1,8 @@
-import Map from "./Map"
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import { useMemo } from "react";
+
 export default function Explore() {
-        const { isLoaded } = useLoadScript({
-          googleMapsApiKey: "AIzaSyAUwo9Bcp-d1hf1Zu6BIPZkFrTB5MjLQIU",
-        });
-       
-        if (!isLoaded) return <div>Loading...</div>;
-        return <MyMap/>;
-       }
-
-function MyMap(){
-    const center = useMemo(() => ({ lat: 44, lng: -80}), []);
     return (
-        <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-          <Marker position={center} />
-        </GoogleMap>
-      );
-    }
-
-
-    // return (
-    //     <>        <h1>This is Explore page</h1>
+        <>        <h1>This is Explore page</h1>
        
-    //     </>
-
-    // );
-    // }
+        </>
+    );
+    }
