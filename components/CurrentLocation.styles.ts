36a212fa2038bcdf115e0067221f7b1f2425679a
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2022-11-27 02:39:00 -0700

    Added current location inside Hawkr Google map

---
+++ components/CurrentLocation.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledBtn = styled.button`
+  position: absolute;
+  z-index: 2;
+  top: 30px;
+  right: 30px;
+  background-color: #4CAF50; /* Green */
+`;
---
+++ components/CurrentLocation.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+// Styles
+import { StyledBtn } from './CurrentLocation.styles';
+
+type Props = {
+  moveTo: (position: google.maps.LatLngLiteral) => void;
+};
+
+function CurrentLocation({moveTo}:Props) {
+  const [disabled, setDisabled] = React.useState(false);
+  console.log("MADE IT INTO THE CURRENT LOCATION FUNCTION");
+  return (
+    <>
+    <StyledBtn
+      disabled={disabled}
+      onClick={() => {
+        // Deactivate button when geolocation is working
+        setDisabled(true);
+        navigator.geolocation.getCurrentPosition(position => {
+          // Activate button when geolocation has finished
+          setDisabled(false);
+          // Call the callback function
+          moveTo({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+          console.log({lag: position.coords.latitude, lng: position.coords.longitude});
+        });
+      }}
+    >
+      {disabled ? <p>Searching ...</p> : <p>Get Current Position</p>}
+    </StyledBtn>
+    </>
+  );
+};
+
+export default CurrentLocation;
