ab6343f545de7931f5324aa4dd848ad5b820a66d
Zixuan Zhang <zixuanzhang1998@gmail.com>
2023-01-10 20:09:38 -0700

    able to jump from the Sign in

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -47,13 +47,13 @@ class Navbar extends React.Component<NavProps, NavState>{
                                     </div>
                                     <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
                                         {/* Current: "border-cyan-400 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                                        <a
+                                        <Link href="/#"
                                             onClick={()=>this.props.handleOpen(true, 1)}
                                             className={classNames(this.props.curr_idx == 1 ? "border-cyan-400 text-gray-900" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
                                                 "inline-flex items-center border-b-4 px-1 pt-1 text-sm font-medium")}
                                         >
                                             Explore Nearby
-                                        </a>
+                                        </Link>
                                         <a
                                             href="/types" onClick={()=>this.props.handleOpen(true, 2)}
                                             className={classNames(this.props.curr_idx == 2 ? "border-cyan-400 text-gray-900" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
