a6144bc3013472ce8969ffc6b0b9f0131221fe74
A_R <u1175124@utah.edu>
2023-01-14 22:49:45 -0700

    signin into a react comp. navbar into in _app file

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,19 +4,105 @@ import Link from 'next/link'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import React from 'react'
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useUser, Session, SupabaseClient, useSession, useSupabaseClient, User } from '@supabase/auth-helpers-react'
+import { preProcessFile } from 'typescript'
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
 
 type NavProps = {
-    handleOpen: (o: boolean, idx:number) => void,
-    curr_idx: number
+    handleOpen: (o: boolean, idx: number) => void,
+    curr_idx: number,
+    session: Session | null
 };
+
+type SignInMenuProps = {
+    session: Session | null
+    // supabase: SupabaseClient
+    // user: User
+}
 
 type NavState = {
 };
+
+type SignInMenuState = {
+}
+
+class SignInMenu extends React.Component<SignInMenuProps, SignInMenuState>{
+
+    constructor(props: SignInMenuProps) {
+        super(props)
+    }
+    render(): React.ReactNode {
+        return (
+            <>
+                {this.props.session ? (
+                    <Link
+                        href="/SignIn"
+                        className={"border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center border-b-4 px-1 pt-1 text-sm font-medium"}
+                    >
+                        Sign In
+                    </Link>) : (
+                    <Menu as="div" className="relative ml-4 flex-shrink-0">
+                        <div>
+                            <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2">
+                                <span className="sr-only">Open user menu</span>
+                                <img
+                                    className="h-8 w-8 rounded-full"
+                                    src="/img/hawkr_icon.png"
+                                    alt=""
+                                />
+                            </Menu.Button>
+                        </div>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                        >
+                                            Your Profile
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                        >
+                                            Settings
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                        >
+                                            Sign out
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                            </Menu.Items>
+                        </Transition>
+                    </Menu>)
+                }
+            </>
+        )
+    }
+}
 
 class Navbar extends React.Component<NavProps, NavState>{
 
@@ -33,7 +119,7 @@ class Navbar extends React.Component<NavProps, NavState>{
                             <div className="flex h-16 justify-between">
                                 <div className="flex px-2 lg:px-0">
                                     <div className="flex flex-shrink-0 items-center">
-                                        <Link href="/#" onClick={()=>this.props.handleOpen(false,0)}>
+                                        <Link href="/#" onClick={() => this.props.handleOpen(false, 0)}>
                                             <img
                                                 className="block h-8 w-auto lg:hidden"
                                                 src="/img/hawkr_icon.png"
@@ -49,14 +135,14 @@ class Navbar extends React.Component<NavProps, NavState>{
                                     <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
                                         {/* Current: "border-cyan-400 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                                         <Link href="/#"
-                                            onClick={()=>this.props.handleOpen(true, 1)}
+                                            onClick={() => this.props.handleOpen(true, 1)}
                                             className={classNames(this.props.curr_idx == 1 ? "border-cyan-400 text-gray-900" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
                                                 "inline-flex items-center border-b-4 px-1 pt-1 text-sm font-medium")}
                                         >
                                             Explore Nearby
                                         </Link>
                                         <a
-                                            href="/#" onClick={()=>this.props.handleOpen(true, 2)}
+                                            href="/#" onClick={() => this.props.handleOpen(true, 2)}
                                             className={classNames(this.props.curr_idx == 2 ? "border-cyan-400 text-gray-900" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
                                                 "inline-flex items-center border-b-4 px-1 pt-1 text-sm font-medium")}
                                         >
@@ -102,69 +188,7 @@ class Navbar extends React.Component<NavProps, NavState>{
                                         <span className="sr-only">View notifications</span>
                                         <BellIcon className="h-6 w-6" aria-hidden="true" />
                                     </button>
-
-                                    {/* Profile dropdown */}
-                                    <Menu as="div" className="relative ml-4 flex-shrink-0">
-                                        <div>
-                                            <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2">
-                                                <span className="sr-only">Open user menu</span>
-                                                <img
-                                                    className="h-8 w-8 rounded-full"
-                                                    src="/img/hawkr_icon.png"
-                                                    alt=""
-                                                />
-                                            </Menu.Button>
-                                        </div>
-                                        <Transition
-                                            as={Fragment}
-                                            enter="transition ease-out duration-100"
-                                            enterFrom="transform opacity-0 scale-95"
-                                            enterTo="transform opacity-100 scale-100"
-                                            leave="transition ease-in duration-75"
-                                            leaveFrom="transform opacity-100 scale-100"
-                                            leaveTo="transform opacity-0 scale-95"
-                                        >
-                                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Your Profile
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Settings
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Sign out
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                            </Menu.Items>
-                                        </Transition>
-                                    </Menu>
-                                    <Link
-                                        href="/SignIn"  onClick={()=>this.props.handleOpen(true, 3)}
-                                        className={classNames(this.props.curr_idx == 3 ? "border-cyan-400 text-gray-900" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                                            "inline-flex items-center border-b-4 px-1 pt-1 text-sm font-medium")}
-                                    >
-                                        Sign In
-                                </Link>
+                                    <SignInMenu session={this.props.session}/>
                                 </div>
                             </div>
                         </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,8 @@ import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import ExploreMenu from '../components/exploreMenu';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import { useUser, useSession, useSupabaseClient, SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+
 import TypesMenu from '../components/typesMenu'
 
 export default function App({ Component, pageProps }: AppProps<{initialSession: Session}>) {
@@ -14,6 +15,8 @@ export default function App({ Component, pageProps }: AppProps<{initialSession: 
   const [exploreOpen, setExploreOpen] = useState(false)
   const [typesOpen, setTypesOpen] = useState(false)
   const [currIdx, setCurIdx] = useState(0)
+
+  const session = useSession()
 
   const setOpen = (o: boolean, idx:number) => {
     if(idx == 1){
@@ -37,7 +40,7 @@ export default function App({ Component, pageProps }: AppProps<{initialSession: 
     initialSession={pageProps.initialSession}
     >
     <div>
-      <Navbar handleOpen={setOpen} curr_idx = {currIdx}/>
+      <Navbar handleOpen={setOpen} curr_idx = {currIdx} session={session}/>
       <ExploreMenu handleOpen={setOpen} open={exploreOpen} />
       <TypesMenu handleOpen={setOpen} open={typesOpen} />
       <Component {...pageProps} />
