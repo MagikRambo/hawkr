19b93ec50efdc4205307e1ef30209ca3b181e05d
Zixuan Zhang <zixuanzhang1998@gmail.com>
2023-01-28 21:17:35 -0700

    Separate code.

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -21,7 +21,13 @@ class ExploreMenu extends React.Component<ExploreMenuProps, ExploreMenuState>{
   };
 
   render(){
-    return (<></>)
+    return (
+    <div className='py-6 px-6'>
+        {/* This is the place to put code*/}
+        <p>This is the juice place</p>
+        {/* This is the place to put code*/}
+    </div>
+    )
   };
 }
 
@@ -38,11 +44,7 @@ class Explore extends React.Component<ExploreProps, ExploreState>{
           <Transition className="w-2/5" show={this.state.showOpen} 
           enter='transition-all' enterFrom='opacity-0 w-0' enterTo='opacity-100 w-2/5'
           leave='transition-all' leaveFrom='opacity-100 w-2/5' leaveTo='opacity-0 w-0'>
-          <div className='py-6 px-6'>
-            {/* This is the place to put code*/}
-            <p>Some good juice right here  d </p>
-            {/* This is the place to put code*/}
-          </div>
+            <ExploreMenu />
           </Transition>
           <div className='relative grow'>
             <button className='absolute z-10 rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm
