2d1615ebb8e2e3b93e04309d3354734f7a3e092b
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2023-02-02 01:16:56 -0700

    working styles with types page

--- components/typesCard.tsx
+++ components/typesCard.tsx
@@ -4,15 +4,14 @@ import { motion } from "framer-motion";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { StarIcon } from "@heroicons/react/24/solid";
 
-type InfoCardProps = {
+type TypesCardProps = {
   img: any;
-  location?: any;
-  description: any;
   title: any;
 };
 
-function InfoCard(ICProps:InfoCardProps) {
+function TypesCard(TCProps:TypesCardProps) {
   return (
+    // <div className="sm:m-1">
     <motion.div
       initial={{
         x: -200,
@@ -21,26 +20,21 @@ function InfoCard(ICProps:InfoCardProps) {
       transition={{ duration: 1 }}
       whileInView={{ opacity: 1, x: 0 }}
       className="flex py-7 px-2 pr-4 border-bi cursor-pointer hover:opacity-80
-    hover:shadow-lg transition duration-200 ease-out first:border-t"
+    hover:shadow-lg transition duration-200 ease-out first:border-t bg-slate-100 sm:m-1 rounded-3xl"
     >
       <div className="relative h-24 w-40 md:h-24 md:w-40 flex-shrink-0">
         <Image
-          src={ICProps.img}
+          src={TCProps.img}
           layout="fill"
           objectFit="cover"
           className="rounded-2xl"
         />
       </div>
-      <div className="flex flex-col flex-grow pl-5">
-        <div className="flex justify-between">
-          <p>{ICProps.location}</p>
-          <HeartIcon className="h-7 cursor-pointer" />
-        </div>
-        <h4 className="text-xl">{ICProps.title}</h4>
-        <div className="border-b w-10 pt-5" />
-        <p className="pt-2 text-sm text-gray-500 flex-grow">{ICProps.description}</p>
+      <div className="flex flex-col flex-grow pl-5 ">
+        <h4 className="text-xl text-black">{TCProps.title}</h4>
       </div>
     </motion.div>
+    // </div>
   );
 };
-export default InfoCard;
+export default TypesCard;
--- pages/types.tsx
+++ pages/types.tsx
@@ -1,6 +1,104 @@
-export default function Types() {
-    return (
-        <h1>This is Types page</h1>
-    )
+import React, { Fragment, useState } from 'react'
+import Map from './Map';
+import { Transition } from '@headlessui/react'
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import getShopsWithLocations from './api/getVendors';
+import TypesCard from '../components/typesCard'
+import {motion} from 'framer-motion'
+import { getHawkrTypeIcon } from '../utils/functions/getHawkrTypeIcon';
+import hawkr_icon from '../public/img/hawkr_icon.png';
+import info from '../utils/info'
+import Link from 'next/link';
+
+
+type ExploreState = {
+  showOpen: boolean;
+};
+
+type TypesMenuProps = {
+  shops:any
 
 }
+
+type TypesMenuState = {
+}
+export const getStaticProps: GetStaticProps = async () => {
+
+  const { data } = await getShopsWithLocations();
+  return { props: { shops: data }};
+};
+
+function TypesMenu (props: TypesMenuProps){
+    
+    let types:any = []
+    let [FoodTruckSet, FoodTruckSetState] = useState(new Set())
+    let [ClothesSet, ClothesSetState] = useState(new Set())
+    let [ArtSet, ArtSetState] = useState(new Set())
+    let HawkrTypeArr = [FoodTruckSet, ClothesSet, ArtSet]
+
+    // var FTSet = []
+    for (let typeElement of props.shops){
+        if (typeElement.hawkrType === "FoodTruck"){
+            FoodTruckSet.add(typeElement)
+        }
+        else if (typeElement.hawkrType === "Clothes"){
+            ClothesSet.add(typeElement)
+        }
+        else if (typeElement.hawkrType ==="Art"){
+            ArtSet.add(typeElement)
+        }
+
+        if (!types.includes(typeElement.hawkrType)){
+            types.push(typeElement.hawkrType)
+        }
+    }
+    return (
+      <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+       viewport={{ once: true }}
+    >
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6 bg-slate-200">
+          <div className="flex flex-col">
+                
+            {types?.map((item: any) => (
+              <TypesCard
+                img={hawkr_icon}
+                title={item}
+              />
+              ))}
+          </div>
+        </section>
+      </main>
+      </motion.div>
+    )
+}
+
+function Types ({shops} : InferGetStaticPropsType<typeof getStaticProps>){
+
+  let [showOpen, setShowOpen] = useState(true)
+
+    // console.log(shops)
+    return (
+      <>
+        <div className="flex justify-between bg-slate-200">
+          <Transition className="w-2/5" show={showOpen} 
+          enter='transition-all' enterFrom='opacity-0 w-0' enterTo='opacity-100 w-2/5'
+          leave='transition-all' leaveFrom='opacity-100 w-2/5' leaveTo='opacity-0 w-0'>
+            <TypesMenu shops={shops}/>
+          </Transition>
+          <div className='relative grow'>
+            <button className='absolute z-10 rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm
+             hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 m-8'
+              onClick={()=>setShowOpen(!showOpen)}>
+              {showOpen ? "Hide Menu" : "Show Menu"}
+            </button>
+            <Map shops={shops}/>
+          </div>
+        </div>
+      </>
+    )
+}
+
+export default Types;
