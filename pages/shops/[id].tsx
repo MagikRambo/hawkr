336becf49aad9b2203ccfbcaf06f26ae530c9360
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2023-02-05 23:11:10 -0700

    integrated with side panel

--- pages/shops/[id].tsx
+++ pages/shops/[id].tsx
@@ -1,8 +1,18 @@
-import React from "react";
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import React, { useState } from 'react'
+import Map from '../Map'
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType} from "next";
 import get_shops_with_location from "../api/getVendors";
 import get_shops_by_id from "../api/getShopById";
+import InfoCard from '../../components/InfoCard'
+import {motion} from 'framer-motion'
+import hawkr_icon from '../../public/img/hawkr_icon.png';
 
+
+import Image from 'next/image';
+import { Transition } from '@headlessui/react'
+import Link from "next/link";
+import LeftArrow from '../../public/img/Left_Arrow.svg'
+import RightArrow from '../../public/img/Right_Arrow.svg'
 
 export const getStaticPaths = async () => {
     const {data} = await get_shops_with_location();
@@ -12,8 +22,6 @@ export const getStaticPaths = async () => {
             params: {id: item.shopID.toString()}
         }
     })
-
-    // console.log(paths)
     return {
         paths,
         fallback: false
@@ -21,7 +29,7 @@ export const getStaticPaths = async () => {
 
 }
 
-export const getStaticProps = async ( {params} ) => {
+export const getStaticProps = async ( {params}:any ) => {
 
 
     const id = params.id
@@ -32,23 +40,71 @@ export const getStaticProps = async ( {params} ) => {
     return {props: {shopData: data}}
 
 }
+function ExploreMenu (props: any){
 
-const Details = ({shopData}:InferGetStaticPropsType<typeof getStaticProps>) => {
+    console.log(props.shops)
+    return (
+      <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+       viewport={{ once: true }}
+    >
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6 bg-slate-200">
+
+          <div className="flex flex-col">
+            {props.shops?.map((item: any) => (
+              <Link href={`shops/${item.shopID}`}>
+              <InfoCard
+                key={item.shopID}
+                img={hawkr_icon}
+                location={"item.location"}
+                description={item.shopDescription}
+                title={item.shopName}
+                />
+                </Link>
+              ))}
+          </div>
+        </section>
+      </main>
+      </motion.div>
+    )
+}
+
+function shopDetails({shopData}:InferGetStaticPropsType<typeof getStaticProps>){
 
     console.log("WE MADE IT TO DETAILS")
 
     console.log(shopData[0])
-    shopData = shopData[0]
-    return(
-        <div>
-            <h1> SAMPLE TEXT BELOW</h1>
-            <h1> {shopData?.shopName}</h1>
-            <p> {shopData?.Description}</p>
-            <p> {shopData?.open}</p>
-            <p> {shopData?.hawkrType}</p>
-            <p> {"shopData.location"}</p>
-        </div>
-    );
+    // shopData = shopData[0]
+    let [showOpen, setShowOpen] = useState(true)
+
+    return (
+        <>
+          <div className="flex justify-between bg-slate-200">
+            <Transition className="w-2/5" show={showOpen} 
+            enter='transition-all' enterFrom='opacity-0 w-0' enterTo='opacity-100 w-2/5'
+            leave='transition-all' leaveFrom='opacity-100 w-2/5' leaveTo='opacity-0 w-0'>
+              <ExploreMenu shops={shopData}/>
+            </Transition>
+            <div className='relative grow'>
+              <button className='absolute z-10 rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm
+               hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 m-8'
+                onClick={()=>setShowOpen(!showOpen)}>
+                {showOpen ? <Image width={23} height={23} alt="LeftArrow" src={LeftArrow.src}/> : <div className="flex text-blue-500 text-base"><Image width={20} height={20} alt="RightArrow" src={RightArrow.src}/>Show Shop</div>}
+              </button>
+              <Map shops={shopData}/>
+            </div>
+          </div>
+        </>
+      )
+}
+function Explore ({shopData} : InferGetStaticPropsType<typeof getStaticProps>){
+
+  let [showOpen, setShowOpen] = useState(true)
+
+    // console.log(shops)
+
 }
 
-export default Details
+export default shopDetails
