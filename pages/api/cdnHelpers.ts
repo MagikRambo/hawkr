3ef32de4ac5c38c172078f1dcf758a45fd615fff
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2023-02-07 01:20:30 -0700

    basic CDN functions, not tested

---
+++ pages/api/cdnHelpers.ts
@@ -0,0 +1,83 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
+import { useState } from "react"
+
+const supabase = useSupabaseClient();
+const [images, setImages] = useState<any>([]);
+
+//https://www.youtube.com/watch?v=8tfdY0Sf2rA&t=210s
+// Video helps with functions above ^
+export async function uploadShopImage(e, user){
+
+    let file = e.target.files[0]
+
+    const {data, error} = await supabase
+    .storage
+    .from('shop_images')
+    .upload(user.id + "/" + file.name, file)
+
+    if(data){
+        console.log("successfully uploaded")
+    }
+    else{
+        console.log(error)
+    }
+}
+
+export async function uploadProfileImage(e,user){
+    let file = e.target.files[0]
+
+    const {data, error} = await supabase
+    .storage
+    .from('profile_images')
+    .upload(user.id + "/" + file.name, file)
+
+    if(data){
+        console.log("successfully uploaded")
+    }
+    else{
+        console.log(error)
+    }
+}
+
+export async function getShopImage(e,user){
+    let file = e.target.files[0]
+
+    const {data, error} = await supabase
+    .storage
+    .from('shop_images')
+    .list(user?.id + "/", {
+        limit: 10,
+        offset: 0,
+        sortBy: {column: "name", order: "asc"}
+    })
+
+
+    if(data !== null){
+        setImages(data);
+    }
+    else{
+        alert("Error loading images")
+        console.log(error)
+    }
+}
+export async function getProfileImage(e,user){
+    let file = e.target.files[0]
+
+    const {data, error} = await supabase
+    .storage
+    .from('shop_images')
+    .list(user?.id + "/", {
+        limit: 10,
+        offset: 0,
+        sortBy: {column: "name", order: "asc"}
+    })
+
+
+    if(data !== null){
+        setImages(data);
+    }
+    else{
+        alert("Error loading images")
+        console.log(error)
+    }
+}
