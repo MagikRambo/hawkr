8bc117019e0b75acac9034d6a2b763d8a8696759
A_R <u1175124@utah.edu>
2023-02-09 22:58:59 -0700

    working dynamic logins.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,15 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import Link from 'next/link'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import React from 'react'
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useSession, useSupabaseClient, useUser } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
+import { Database } from '../utils/database.types'
+type Profiles = Database['public']['Tables']['profiles']['Row']
 import TypesMenu from './typesMenu'
+import { Router, useRouter } from 'next/router'
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -26,6 +29,41 @@ function Navbar (){
     const [exploreOpen, setExploreOpen] = useState(false)
     const [typesOpen, setTypesOpen] = useState(false)  
     const [currIdx, setCurIdx] = useState(0)
+    const router = useRouter()
+    const session = useSession()
+    const supabase = useSupabaseClient()
+
+    // Start Supabase user code
+    // const user = useUser()
+    // const [loading, setLoading] = useState(true)
+    // const [username, setUsername] = useState<Profiles['name']>(null)
+  
+    // async function getProfile() {
+    //   try {
+    //     setLoading(true)
+    //     if (!user) throw new Error('No user')
+  
+    //     let { data, error, status } = await supabase
+    //       .from('profiles')
+    //       .select(`name`)
+    //       .eq('id', user.id)
+    //       .single()
+  
+    //     if (error && status !== 406) {
+    //       throw error
+    //     }
+  
+    //     if (data) {
+    //       setUsername(data.name)
+    //     }
+    //   } catch (error) {
+    //     alert('Error loading user data!')
+    //     console.log(error)
+    //   } finally {
+    //     setLoading(false)
+    //   }
+    // }
+    // End Supabase user code
     
     const setOpen = (o: boolean, idx:number) => {
         console.log(o,idx)
@@ -114,112 +152,99 @@ function Navbar (){
                                     </Disclosure.Button>
                                 </div>
                                 <div className="hidden lg:ml-4 lg:flex lg:items-center">
-                                    <button
-                                        type="button"
-                                        className="flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2"
-                                    >
-                                        <span className="sr-only">View notifications</span>
-                                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                                    </button>
-
-                                    {/* Profile dropdown */}
-                                    <Menu as="div" className="relative ml-4 flex-shrink-0">
-                                        <div>
-                                            <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2">
-                                                <span className="sr-only">Open user menu</span>
-                                                <img
-                                                    className="h-8 w-8 rounded-full"
-                                                    src="/img/hawkr_icon.png"
-                                                    alt=""
-                                                />
-                                            </Menu.Button>
-                                        </div>
-                                        <Transition
-                                            as={Fragment}
-                                            enter="transition ease-out duration-100"
-                                            enterFrom="transform opacity-0 scale-95"
-                                            enterTo="transform opacity-100 scale-100"
-                                            leave="transition ease-in duration-75"
-                                            leaveFrom="transform opacity-100 scale-100"
-                                            leaveTo="transform opacity-0 scale-95"
-                                        >
-                                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Your Profile
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Settings
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Sign out
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                            </Menu.Items>
-                                        </Transition>
-                                    </Menu>
+                                    {! session? (
                                     <Link
                                         href="/SignIn"  onClick={()=>setOpen(true, 3)}
                                         className={classNames(currIdx == 3 ? "border-cyan-400 text-gray-900" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
                                             "inline-flex items-center border-b-4 px-1 pt-1 text-sm font-medium")}
-                                    >
-                                        Sign In
-                                </Link>
+                                            >
+                                                Sign In
+                                    </Link>) : (
+                                       <>
+                                       <button
+                                            type="button"
+                                            className="flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2"
+                                        >
+                                            <span className="sr-only">View notifications</span>
+                                            <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                        </button>
+
+                                        {/* Profile dropdown */}
+                                        <Menu as="div" className="relative ml-4 flex-shrink-0">
+                                            <div>
+                                                <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2">
+                                                    <span className="sr-only">Open user menu</span>
+                                                    <img
+                                                        className="h-8 w-8 rounded-full"
+                                                        src="/img/hawkr_icon.png"
+                                                        alt=""
+                                                    />
+                                                </Menu.Button>
+                                            </div>
+                                            <Transition
+                                                as={Fragment}
+                                                enter="transition ease-out duration-100"
+                                                enterFrom="transform opacity-0 scale-95"
+                                                enterTo="transform opacity-100 scale-100"
+                                                leave="transition ease-in duration-75"
+                                                leaveFrom="transform opacity-100 scale-100"
+                                                leaveTo="transform opacity-0 scale-95"
+                                            >
+                                                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                    <Menu.Item>
+                                                        {({ active }) => (
+                                                            <a
+                                                                href="#"
+                                                                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                            >
+                                                                Your Profile
+                                                            </a>
+                                                        )}
+                                                    </Menu.Item>
+                                                    <Menu.Item>
+                                                        {({ active }) => (
+                                                            <a
+                                                                href="#"
+                                                                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                            >
+                                                                Settings
+                                                            </a>
+                                                        )}
+                                                    </Menu.Item>
+                                                    <Menu.Item>
+                                                        {({ active }) => (
+                                                            <a
+                                                                href="#"
+                                                                onClick={() => {supabase.auth.signOut()} }
+                                                                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                            >
+                                                                Sign out
+                                                            </a>
+                                                        )}
+                                                    </Menu.Item>
+                                                </Menu.Items>
+                                            </Transition>
+                                        </Menu>
+                                       </> 
+                                    )}
                                 </div>
                             </div>
                         </div>
-
-                        <Disclosure.Panel className="lg:hidden">
-                            <div className="space-y-1 pt-2 pb-3">
-                                {/* Current: "bg-cyan-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" */}
-                                <Disclosure.Button
-                                    as="a"
-                                    href="#"
-                                    className="block border-l-4 border-cyan-400 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
-                                >
-                                    Dashboard
-                                </Disclosure.Button>
-                                <Disclosure.Button
-                                    as="a"
-                                    href="#"
-                                    className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
-                                >
-                                    Team
-                                </Disclosure.Button>
-                                <Disclosure.Button
-                                    as="a"
-                                    href="#"
-                                    className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
-                                >
-                                    Projects
-                                </Disclosure.Button>
-                                <Disclosure.Button
-                                    as="a"
-                                    href="#"
-                                    className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
-                                >
-                                    Calendar
-                                </Disclosure.Button>
-                            </div>
+                        {/* For smaller screens */}
+                        {! session? (
+                            <Disclosure.Panel className="lg:hidden">
+                                <div className="border-t border-gray-200 pt-4 pb-3">
+                                    <Link
+                                        href="/SignIn"  onClick={()=>setOpen(true, 3)}
+                                        className={classNames(currIdx == 3 ? "border-cyan-400 text-gray-900" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                                            "inline-flex items-center border-b-4 px-1 pt-1 text-sm font-medium")}
+                                            >
+                                                Sign In
+                                    </Link>
+                                </div>
+                            </Disclosure.Panel>
+                        ) : (
+                            <Disclosure.Panel className="lg:hidden">
                             <div className="border-t border-gray-200 pt-4 pb-3">
                                 <div className="flex items-center px-4">
                                     <div className="flex-shrink-0">
@@ -257,15 +282,15 @@ function Navbar (){
                                         Settings
                                     </Disclosure.Button>
                                     <Disclosure.Button
-                                        as="a"
-                                        href="#"
+                                        
+                                        onClick={() => {supabase.auth.signOut(); router.push('./')} }
                                         className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
                                     >
                                         Sign out
                                     </Disclosure.Button>
                                 </div>
                             </div>
-                        </Disclosure.Panel>
+                        </Disclosure.Panel>)}
                     </>
                 )}
             </Disclosure>
