11248838ba0f3bf9cebad97f4d5bf5a78df3045f
Zixuan Zhang <zixuanzhang1998@gmail.com>
2023-02-17 19:43:03 -0700

    Add profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,12 @@
+
+type ProfileProps = {
+  }
+
+export default function ShopCard(props: ProfileProps){
+
+  return (
+    <main className="flex">
+        <h1>This is the Profile Props</h1>
+    </main>
+  )
+}
