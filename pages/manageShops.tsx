210262b58742eb2daca897b603589fb435767503
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2023-03-09 22:14:40 -0700

    adding tailwind Elements and pre adding time styling

--- pages/manageShops.tsx
+++ pages/manageShops.tsx
@@ -70,11 +70,13 @@ export default function manageShops(){
                         </div>
                     </div>)}
                     {showModal ? 
-    <div className="overflow-scroll bg-white rounded-xl h-[75vh] w-[75vw] border-4 box-shadow-md">
+    <div className="overflow-scroll bg-white rounded-xl h-[75vh] w-[75vw] shadow-xl">
     <div className="flex justify-end">    
-        <button onClick={() => setShowModal(false)} className=" absolute bg-red-200 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
-            X
-        </button>
+    {/* Dark color for button:  dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 */}
+    <button type="button" onClick={() => setShowModal(false)} className="absolute ml-auto mx-1.5 my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8" data-dismiss-target="#toast-default" aria-label="Close">
+        <span className="sr-only">Close</span>
+        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </button>
     </div>
     <div  className="box-content h-4/5 w-4/5">
 
@@ -317,7 +319,7 @@ export default function manageShops(){
             </div>
         </div>
 
-        <div className="pt-5">
+        <div className="py-5">
             <div className="flex justify-end">
             <button
                 type="button"
