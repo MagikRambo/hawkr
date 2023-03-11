c9e36cf52c044f4c9abce04513f1de122364ac15
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2023-03-11 02:49:47 -0700

    able to read attribtues in backend

--- pages/api/newShop.ts
+++ pages/api/newShop.ts
@@ -1,50 +1,78 @@
 import { uploadShopImage } from "./cdnHelpers"
 import { useSupabaseClient } from "@supabase/auth-helpers-react"
-import formidable from 'formidable'
+// Backend
+import formidable from 'formidable';
 
-export default async function (req, res) {
-    // require("env.local").config();
-//   const supabase = useSupabaseClient();
-  const body =  req.body
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
-  //  console.log('body: ' , body)
-  //  console.log('file: ' , body.file)
-  //  console.log('shopName: ' , body.shopName)
-//    console.log('shopTitle: ' , body[0].shopDescription)
-  // const form = new formidable.IncomingForm();
-  // form.uploadDir = "./";
-  // form.keepExtensions = true;
-  // form.parse(req, (err, fields, files) => {
-  //   console.log('form parsing: ', err, fields, files);
-  // });
-   if (!body.first || !body.last) {
-    // Sends a HTTP bad request error code
-    return res.status(400).json({ data: 'First or last name not found' })
-  }
+export default async (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.uploadDir = "./";
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err){
+      res.status(400).json({ status: `Error: ${err}` });
+    }
 
-    // // "use strict";
-    // let nodemailer = require("nodemailer");
-    // const transporter = nodemailer.createTransport({
-    //   port: 465,
-    //   host: "smtp.gmail.com",
-    //   auth: {
-    //     user: email,
-    //     pass: pass
-    //   },
-    //   secure: true,
-    // });
+    //THIS IS GOOD!
+    // console.log('file content: ', files.file)
 
-    await new Promise((resolve, reject) => {
-    //   transporter.sendMail(mailData, function (err, info) {
-    //     if (err) {
-    //       console.log(err);
-    //       reject(err);
-    //     } else {
-    //       console.log(info);
-    //       resolve(info);
-    //     }
-    //   });
-    });
-    res.status(200).json({ status: "OK" });
-  }
+    
+
+    console.log(err, fields, files);
+  });
+  res.status(200).json({ status: "OK" });
+
+};
+
+
+// export default async function (req, res) {
+//     // require("env.local").config();
+// //   const supabase = useSupabaseClient();
+//   const body =  req.body
+
+//    console.log('body: ' , {body})
+//   //  console.log('file: ' , body.file)
+//   //  console.log('shopName: ' , body.shopName)
+// //    console.log('shopTitle: ' , body[0].shopDescription)
+//   // const form = new formidable.IncomingForm();
+//   // form.uploadDir = "./";
+//   // form.keepExtensions = true;
+//   // form.parse(req, (err, fields, files) => {
+//   //   console.log('form parsing: ', err, fields, files);
+//   // });
+//    if (!body.first || !body.last) {
+//     // Sends a HTTP bad request error code
+//     return res.status(400).json({ data: 'First or last name not found' })
+//   }
+
+//     // // "use strict";
+//     // let nodemailer = require("nodemailer");
+//     // const transporter = nodemailer.createTransport({
+//     //   port: 465,
+//     //   host: "smtp.gmail.com",
+//     //   auth: {
+//     //     user: email,
+//     //     pass: pass
+//     //   },
+//     //   secure: true,
+//     // });
+
+//     await new Promise((resolve, reject) => {
+//     //   transporter.sendMail(mailData, function (err, info) {
+//     //     if (err) {
+//     //       console.log(err);
+//     //       reject(err);
+//     //     } else {
+//     //       console.log(info);
+//     //       resolve(info);
+//     //     }
+//     //   });
+//     });
+//     res.status(200).json({ status: "OK" });
+//   }
   
