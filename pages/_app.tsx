0e14ddcfb43e0bec217a2c693c79c4ccf8a2b4f9
Zixuan Zhang <zixuanzhang1998@gmail.com>
2023-03-29 21:26:31 -0600

    update locationtimer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,47 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 
-export default function App({ Component, pageProps }: AppProps<{initialSession: Session}>) {
-  
+export default function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
+
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
-  
+  const [timer, setTimer] = useState(0);
+
+  const interval = 1000;
+
+  const handleTimer = () => {
+    // handle Timer is able to be called by every second
+    console.log('Function called every second');
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimer((prevTimer) => {
+        handleTimer();
+        return prevTimer + interval;
+      });
+    }, interval);
+
+    // clear timer
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <SessionContextProvider
-    supabaseClient={supabaseClient}
-    initialSession={pageProps.initialSession}
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
     >
-    <div>
-      <Navbar/>
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+      <div>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
     </SessionContextProvider>
   )
 }
