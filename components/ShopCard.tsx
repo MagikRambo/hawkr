499a5dcd3253d6f6078712d866fcd080bc746dc6
mike marambio <u1229804@utah.edu>
2023-04-01 13:17:50 -0600

    added api calls on toggle to update db

--- components/ShopCard.tsx
+++ components/ShopCard.tsx
@@ -1,7 +1,8 @@
 import { HeartIcon, StarIcon } from '@heroicons/react/24/outline';
+import { useUser } from '@supabase/auth-helpers-react';
 // import { HeartIcon } from '@heroicons/react/24/solid';
 import { useState } from 'react';
-
+import { supabase } from '../utils/supabaseClient';
 type ShopCardProps = {
     hawkrType: string,
     location: {lat: number, lng: number},
@@ -14,15 +15,36 @@ type ShopCardProps = {
   // <!-- Please refer: https://github.com/shubhamjain/svg-loader -->
 export default function ShopCard(props: ShopCardProps){
 
-  const [HeartStatus, setHeartStatus] = useState<boolean>(true)
+  const [HeartStatus, setHeartStatus] = useState<boolean>(false)
 
-  function toggleHeart(){
+  const user = useUser();
+  const userID = user?.id
+
+  async function toggleHeart(shopID:string){
     if (HeartStatus){
       setHeartStatus(false)
+      removeFromFavorites(shopID)
     }else{
       setHeartStatus(true)
+      insertFavorites(shopID)
     }
   }
+
+
+const insertFavorites = async (shopID:string) => {
+  const { data, error } = await supabase
+  .from('favoritesList')
+  .insert([
+    { clientID: userID, shopID: shopID },
+])
+}
+
+const removeFromFavorites  = async(shopID:string) => {
+  const { data, error } = await supabase
+  .from('favoritesList')
+  .delete()
+  .match({clientID: userID, shopID: shopID})
+}
 
   return (
     <main className="flex">
@@ -33,7 +55,11 @@ export default function ShopCard(props: ShopCardProps){
             <h1 className='text-6xl font-bold pt-8'>{props.shopName}</h1>
             <p className='text-xl pb-4'>{props.hawkrType}</p>
           </div>
-          {HeartStatus ? <button onClick={toggleHeart}><HeartIcon className='h-14 cursor-pointer mr-7 '/></button> : <button onClick={toggleHeart}><svg className="h-14 cursor-pointer mr-2 " width="96" height="96" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" color="#ef4444"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"></path></svg></button>
+
+          {HeartStatus ? 
+          <button onClick={() => toggleHeart(props.shopID)}><svg className="h-14 cursor-pointer mr-2 " width="96" height="96" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" color="#ef4444"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"></path></svg></button> 
+          : 
+          <button onClick={() => toggleHeart(props.shopID)}><HeartIcon className='h-14 cursor-pointer mr-7 '/></button>
           }
         </div>
         <p className='font-bold text-xl py-2'>Current Location: {props.location.lat} {props.location.lng}</p>
