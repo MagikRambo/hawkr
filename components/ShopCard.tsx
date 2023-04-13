c65f49b20b36ce90c1394b284b2ee6a2c62a3e8b
mike marambio <u1229804@utah.edu>
2023-04-12 21:23:03 -0600

    showcasing other features based on user status

--- components/ShopCard.tsx
+++ components/ShopCard.tsx
@@ -81,7 +81,7 @@ const retrieveFavoritesList = async () => {
 }
 
   return (
-    <main className="flex text-slate-950">
+    <main className="flex text-slate-950 text-black">
       <section className='flex-grow w-screen h-screen px-2 sm:px-16 bg-slate-200 overflow-y-auto [&::-webkit-scrollbar]:hidden'>
       {/* ~~~~~~~ EDITS NEED TO BE MADE HERE FOR IMAGE ~~~~~~~ */}
       <div className='will-change-auto relative'>
@@ -92,18 +92,20 @@ const retrieveFavoritesList = async () => {
             <h1 className='text-3xl sm:text-6xl font-bold pt-8'>{props.shopName}</h1>
             <p className='text-xl pb-4'>{props.hawkrType}</p>
           </div>
-
-          {HeartStatus ? 
+          { user && (
+          HeartStatus ? 
           <button onClick={() => toggleHeart(props.shopID)}><svg className="h-14 cursor-pointer mr-2 " width="96" height="96" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" color="#ef4444"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"></path></svg></button> 
           : 
           <button onClick={() => toggleHeart(props.shopID)}><HeartIcon className='h-14 cursor-pointer mr-7 '/></button>
-          }
+          )}
         </div>
         <p className='font-bold text-xl py-2'>Current Location: {props.location.lat} {props.location.lng}</p>
         <p className='font-bold text-2xl sm:text-4xl py-4'>Vender Detail</p>
         <p className='py-2 text-2xl'>{props.shopDescription}</p>
 
         {/** The text Review Part*/}
+        {user && 
+        <>
         <p className='font-bold text-2xl sm:text-4xl py-4'>Leave a Review</p>
         <div className='flex justify-start'>
           <StarIcon className="h-10 cursor-pointer" />
@@ -121,6 +123,8 @@ const retrieveFavoritesList = async () => {
           Post
         </button>
       </div>
+      </>
+      }
       </section>
     </main>
   )
---
+++ pages/about.tsx
@@ -0,0 +1,24 @@
+
+
+
+export default function about(){
+
+
+    return(
+        <>
+        <header>
+            <title> About us!</title>
+        </header>
+
+        <main className="h-screen w-full bg-slate-200 text-black">
+
+        <h1 className="text-4xl">About us!</h1>
+
+        <h1>Mike Marambio | Alex Romero | Leo Zhang</h1>
+
+        </main>
+        </>
+    )
+
+
+}
