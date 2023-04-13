a8f236979693bafb3c842b226545158461531c6e
mike marambio <u1229804@utah.edu>
2023-04-12 23:07:11 -0600

    about section pre

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,24 +1,110 @@
 
-
-
-export default function about(){
-
-
-    return(
-        <>
-        <header>
-            <title> About us!</title>
-        </header>
-
-        <main className="h-screen w-full bg-slate-200 text-black">
-
-        <h1 className="text-4xl">About us!</h1>
-
-        <h1>Mike Marambio | Alex Romero | Leo Zhang</h1>
-
-        </main>
-        </>
+const people = [
+    {
+      name: 'Mike Marambio',
+      role: 'Full-Stack Developer',
+      description: 'TContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32',
+      imageUrl:
+        'https://www.mikemarambio.com/images/profile.jpg',
+      linkedinUrl: '#',
+    },
+    {
+        name: 'Alex Romero',
+        role: 'Front-end Developer',
+        description: 'Text goes here',
+        imageUrl:
+          'https://mlijczvqqsvotbjytzjm.supabase.co/storage/v1/object/public/base-hawkr/hawkr_icon.png',
+        linkedinUrl: 'https://www.linkedin.com/in/mike-marambio/',
+      },
+      {
+        name: 'Leo Zhang',
+        role: 'Front-end Developer ',
+        description: 'Text goes here' ,
+        imageUrl:
+          'https://mlijczvqqsvotbjytzjm.supabase.co/storage/v1/object/public/base-hawkr/hawkr_icon.png',
+        linkedinUrl: '#',
+      },
+  ]
+  
+  export default function Example() {
+    return (
+      <div className="bg-slate-200 py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our team</h2>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the
+              best results for our clients.
+            </p>
+          </div>
+          <ul
+            role="list"
+            className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+          >
+            {people.map((person) => (
+              <li key={person.name}>
+                <img className="aspect-[2/2] w-full rounded-2xl object-cover" src={person.imageUrl} alt="" />
+                <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">{person.name}</h3>
+                <p className="text-base leading-7 text-gray-600 pb-4">{person.role}</p>
+                <p className="text-base leading-7 text-gray-600">{person.description}</p>
+                <ul role="list" className="mt-6 flex gap-x-6">
+                  {/* <li>
+                    <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
+                      <span className="sr-only">Twitter</span>
+                      <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                      </svg>
+                    </a>
+                  </li> */}
+                  <li>
+                    <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
+                      <span className="sr-only">LinkedIn</span>
+                      <svg className="h-7 w-7" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path
+                          fillRule="evenodd"
+                          d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     )
+  }
+  
 
 
-}
+
+// export default function about(){
+
+
+//     return(
+//         <>
+//         <header>
+//             <title> About us!</title>
+//         </header>
+
+//         <main className="h-screen w-full bg-slate-200 text-black">
+
+//         <h1 className="text-4xl">About us!</h1>
+
+//         <h1> Hawkr Created by </h1>
+
+//         {/* Grid of 3 separated */}
+//         <div>
+
+
+//         </div>
+//         <h1>Mike Marambio | Alex Romero | Leo Zhang</h1>
+
+//         </main>
+//         </>
+//     )
+
+
+// }
