8945778de3ae830726ca2066fb06209fb5b7b6ef
mike marambio <u1229804@utah.edu>
2023-04-14 00:07:20 -0600

    added alex profile

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,9 +13,9 @@ const people = [
         role: 'Front-end Developer',
         description: 'Text goes here',
         imageUrl:
-          'https://mlijczvqqsvotbjytzjm.supabase.co/storage/v1/object/public/base-hawkr/hawkr_icon.png',
-        linkedinUrl: '',
-        email: 'alexromero@gmail.com'
+          ' https://alex-romero.vercel.app/selfie.jpg',
+        linkedinUrl: 'https://www.linkedin.com/in/alex--romero/',
+        email: 'alexander--romero@outlook.com'
       },
       {
         name: 'Leo Zhang',
@@ -48,8 +48,9 @@ const people = [
                 <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">{person.name}</h3>
                 <p className="text-base leading-7 text-gray-600 pb-4">{person.role}</p>
 
-                {person.name === 'Mike Marambio' && (
                 <div className="text-black">
+                {person.name === 'Mike Marambio' && (
+                    <>
                     <p className="pb-4">My name is Mike Marambio, and I am a Computer Science student at the University of Utah, set to graduate in the spring of 2023. As an aspiring software engineer, I am passionate about building scalable and efficient systems, particularly in the areas of Microservices, Distributed Systems, and Containerization.</p>
                     
                     <p className="pb-4"> In addition to my academic pursuits, I am also an undergraduate researcher at the Visual Perception and Spatial Cognition Lab, where I focus on exploring the potential of Mixed Reality. Furthermore, I am honored to serve as Vice President of The Society of Hispanic Professional Engineers (SHPE), a role that has allowed me to connect with fellow students and professionals in the engineering field while promoting diversity and inclusion. </p>
@@ -57,20 +58,24 @@ const people = [
                     <p className="pb-4">My interest in software development has also led me to intern at Microsoft twice. During my first internship, I developed an admin UI and later pivoted the project towards a Full-Stack direction. In my second internship, I created a developer tool to simplify containerization and improve the developer life cycle.</p>
                     
                     <p className="pb-4">When I'm not immersed in coding, I enjoy training, learning new hobbies, and spending quality time with family and friends. After graduation, I plan to continue developing my current project and integrate additional features to benefit small local vendors. I am confident that my skills and experiences will enable me to make a meaningful impact in the software engineering industry.</p>
-                 </div>)}
+                    </>)}
                  {person.name === 'Alex Romero' && (
-                    <p>example text here</p>
-                 )}
+                    <>
+                    <p className="pb-4">My name is Alex, I am a Hispanic American studying at the University of Utah. This year I am graduating with a bachelor’s degree in computer science and am currently in search of software engineering positions.</p>
+                    <p className="pb-4">During my time as an undergraduate, I have gained experience with Java, Kotlin, C/C++, C#, Python, JavaScript, React, and JavaScript. </p>
+                    <p className="pb-4">I previously worked for Blendtec in Orem as a Software/Systems Engineer for approximately 3 years. While my experiences are diverse and with several different systems, I have a strong interest in OpSec and Backend Development. I have several hobbies including hiking, camping, automobile mechanics, and music discovery.</p>
+                    </>)}
 
                  {person.name === 'Leo Zhang' && (
-                <div className="text-black">
+                    <>
                     <p className="pb-4">Hello my name is Leo, born on Feb  19, 1998 in Tianjin. By chance, I came to the University of Utah and had the most memorable time of my life here.</p>
                     
-                    <p className="pb-4">I pursued architecture design in college at beginning, but soon realized my growing interest in computer science and computer graphics. I made the decision to continue his studies in the field at the University of Utah in the United States, and thanks for this choose, I meet a lot of cool guys here. </p>
+                    <p className="pb-4">I pursued architecture design in college at beginning, but soon realized my growing interest in computer science and computer graphics. I made the decision to continue my studies in the field at the University of Utah in the United States, and thanks for this choose, I meet a lot of cool guys here. </p>
                     
                     <p className="pb-4">As my undergraduate studies progressed, I became increasingly interested in graphics. I enjoy reading about rendering and the technologies associated with game engines. My biggest dream is to open my own game studio and dedicate myself to providing a better gaming experience for players.</p>                                    
-                </div>)}
-                <p className="text-base leading-7 text-gray-600">{person.email}</p>
+                    </>)}
+                </div>
+                <p className="text-base leading-7 text-gray-600">Email: {person.email}</p>
                 <ul role="list" className="mt-6 flex gap-x-6">
                   <li>
                     <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
@@ -93,34 +98,3 @@ const people = [
     )
   }
   
-
-
-
-// export default function about(){
-
-
-//     return(
-//         <>
-//         <header>
-//             <title> About us!</title>
-//         </header>
-
-//         <main className="h-screen w-full bg-slate-200 text-black">
-
-//         <h1 className="text-4xl">About us!</h1>
-
-//         <h1> Hawkr Created by </h1>
-
-//         {/* Grid of 3 separated */}
-//         <div>
-
-
-//         </div>
-//         <h1>Mike Marambio | Alex Romero | Leo Zhang</h1>
-
-//         </main>
-//         </>
-//     )
-
-
-// }
